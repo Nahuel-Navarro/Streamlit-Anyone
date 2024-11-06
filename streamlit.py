3fbe5c52f6451722f68f6579de507d8704a80f24
Nahuel <nahunavarro07@gmail.com>
2024-11-06 20:19:05 -0300

    Second commit

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from sklearn.datasets import load_wine
 
+
+data = load_wine(as_frame = True)
+df = data.frame
+
+st.title("Aguante streamlit")
 st.header("Streamlit Test")
+
+st.dataframe()
